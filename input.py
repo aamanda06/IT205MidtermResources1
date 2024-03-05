09fa6dc76ce603ec1375f52bc8bccb0f907511dd
aamanda06 <159112489+aamanda06@users.noreply.github.com>
2024-03-05 15:44:18 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,12 @@
+x = 25
+message ="Hi"
+ratio = 3.14
+
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+
+
+print(message + "My name is Amanda. I am" + x + "years old.")
+print(f"{message} my name is Amanda I am {x} years old.")
